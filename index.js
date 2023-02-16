3c6a9da2bda908d7d1a04b93781437ef681bc996
Joaquin Rosendo <joaquinrosendo@live.com>
2023-02-16 15:12:32 -0600

    render food items from data.js

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 import {menuArray} from './data.js';
 
-console.log(menuArray);
 
+let renderMenu = (menuArray) => {
+
+    let htmlMenu = ``;
+
+    menuArray.map(menuItem => {
+        console.log(menuItem);
+
+        htmlMenu += `<div class="menu-item">
+                        <div class="item-info-container">
+                            <p class="item-emoji">${menuItem.emoji}</p>
+                            <article class="item-description">
+                                <p class="item-name">${menuItem.name}</p>
+                                <p class="item-ingredients">${menuItem.ingredients}</p>
+                                <p class="item-price">${menuItem.price}</p>
+                            </article>
+                        </div>
+                        <span>
+                            <div class="add-item-btn">
+                                <p>+</p>
+                            </div>
+                        </span>
+                    </div>`
+    })
+
+    document.getElementById("menu-container").innerHTML = htmlMenu;
+}
+
+renderMenu(menuArray);
